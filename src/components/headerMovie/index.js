0c78eb7e24197aed0dd06ca67ead53eb88c885b8
Berncat <b.cattigan1@hotmail.com>
2022-07-16 16:34:32 +0100

    Fix backward/foeward arrows

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -6,6 +6,7 @@ import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import HomeIcon from "@material-ui/icons/Home";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,15 +22,15 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const MovieHeader = (props) => {
+  const navigate = useNavigate();
   const classes = useStyles();
   const movie = props.movie;
 
   return (
     <Paper component="div" className={classes.root}>
-      <IconButton aria-label="go back">
+      <IconButton aria-label="go back" onClick={() => navigate(-1)}>
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
-
       <Typography variant="h4" component="h3">
         {movie.title}
         <a href={movie.homepage}>
@@ -38,7 +39,7 @@ const MovieHeader = (props) => {
         <br />
         <span className={classes.tagLine}>{`   "${movie.tagline}"`} </span>
       </Typography>
-      <IconButton aria-label="go forward">
+      <IconButton aria-label="go forward" onClick={() => navigate(1)}>
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
--- src/components/headerMovieList/index.js
+++ src/components/headerMovieList/index.js
@@ -5,6 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,23 +17,19 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Header = (props ) => {
+const Header = (props) => {
+  const navigate = useNavigate();
   const classes = useStyles();
-  const title = props.title
+  const title = props.title;
   return (
     <Paper component="div" className={classes.root}>
-      <IconButton
-        aria-label="go back"
-      >
+      <IconButton aria-label="go back" onClick={() => navigate(-1)}>
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
-
       <Typography variant="h4" component="h3">
         {title}
       </Typography>
-      <IconButton
-        aria-label="go forward"
-      >
+      <IconButton aria-label="go forward" onClick={() => navigate(1)}>
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
