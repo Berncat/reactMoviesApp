b49c619f0cfff1009b6c4e4e4da1e7fcb13958ae
Berncat <b.cattigan1@hotmail.com>
2022-07-17 14:49:51 +0100

    Change upcoming movies page to use caching.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -76,13 +76,17 @@ export const getMovieReviews = (id) => {
     });
 };
 
-export const getUpcomingMovies = (id) => {
+export const getUpcomingMovies = () => {
   return fetch(
     `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
   )
-    .then((res) => res.json())
-    .then((json) => {
-      // console.log(json.results);
-      return json.results;
-    });
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+    throw error;
+  });
 };
--- src/pages/upcomingPage.js
+++ src/pages/upcomingPage.js
@@ -1,17 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTemplate from "../components/templateMovieListPage";
+import { useQuery } from "react-query";
+import Spinner from "../components/spinner";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import AddToMustWatchIcon from "../components/cardIcons/addToMustWatch";
 
 const UpcomingPage = (props) => {
-  const [movies, setMovies] = useState([]);
+  const { data, error, isLoading, isError } = useQuery("upcoming", getUpcomingMovies);
 
-  useEffect(() => {
-    getUpcomingMovies().then((movies) => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+  const movies = data.results;
 
   return (
     <PageTemplate
