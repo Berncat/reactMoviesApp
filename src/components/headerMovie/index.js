643a099ade675c4219f9ac2c91e1d99afb198295
Berncat <b.cattigan1@hotmail.com>
2022-08-14 03:59:06 +0100

    Updated movie detail header

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -5,7 +5,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import HomeIcon from "@material-ui/icons/Home";
 import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
@@ -14,7 +13,8 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "space-around",
     alignItems: "center",
     flexWrap: "wrap",
-    padding: theme.spacing(1.5),
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
   },
   tagLine: {
     fontSize: "1.5rem",
@@ -27,15 +27,12 @@ const MovieHeader = (props) => {
   const movie = props.movie;
 
   return (
-    <Paper component="div" className={classes.root}>
+    <Paper component="div" className={classes.root} elevation={24}>
       <IconButton aria-label="go back" onClick={() => navigate(-1)}>
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
       <Typography variant="h4" component="h3">
-        {movie.title}
-        <a href={movie.homepage}>
-          <HomeIcon color="primary" />
-        </a>
+        <a href={movie.homepage}>{movie.title}</a>
         <br />
         <span className={classes.tagLine}>{`   "${movie.tagline}"`} </span>
       </Typography>
